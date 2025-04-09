ae2e00dcfce61010fca092d8794c1d6cdb03b570
Sungwoo Park <glapark@gmail.com>
2025-04-09 14:59:46 +0900

    Fix editUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -40,7 +40,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/mr3project/mr3docs2/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/mr3project/mr3docs2/tree/main/',
         },
         blog: {
           showReadingTime: true,
